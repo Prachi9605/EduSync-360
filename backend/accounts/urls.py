41f0a25e8581703ad817e3c81e7b9b3b4cebfae1
Prachi Kadam <kadamprachi39448@gmail.com>
2025-10-03 11:23:47 +0530

    Delete backend directory

--- backend/accounts/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import CustomAuthToken
-
-urlpatterns = [
-    path("login/", CustomAuthToken.as_view(), name="api_login"),
-]
--- backend/core/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-
-urlpatterns = [
-    path("students/", include("students.urls")),
-    path("teachers/", include("teachers.urls")),
-    path("adminpanel/", include("adminpanel.urls")),
-]
